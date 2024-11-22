940fc01b8b0bcc78ef882105953c410ed116f572
Julio Cesar Rodrigues dos Reis <julioreis044@gmail.com>
2024-11-22 16:26:11 -0300

    Configurando migrations

---
+++ migrations/20241122183042-create-drivers.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('drivers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      vehicle: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      rating: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      rate_per_km: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      min_km: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('drivers');
+  }
+};
--- src/controllers/RegisterTrips/Create.ts
+++ src/controllers/RegisterTrips/Create.ts
@@ -2,7 +2,15 @@ import { Request, Response } from "express";
 
 
 
+export const create = async (req: Request, res: Response) => {
 
-export const create = (req: Request, res: Response) => {
+    const data = req.body;
+
+    if(!data) { 
+        res.status(400).json({ error: 'Todos os campos são obrigatórios.' })
+    }else{
+        res.send(data).json;
+    };
+
     res.send('Viagem criada!!!');
 };
